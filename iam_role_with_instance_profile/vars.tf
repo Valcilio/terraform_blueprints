9f3a0d0e03d31909b8f15d45b647ff456b89c66b
Valcilio <eugeniovalcilio@gmail.com>
2024-01-06 15:55:23 -0300

    correcting terraform formats 0.2

--- iam_role_with_instance_profile/vars.tf
+++ iam_role_with_instance_profile/vars.tf
@@ -1,21 +1,21 @@
 variable "role_name" {
   description = "The name of the role which will be created"
-  type = string
+  type        = string
 }
 
 variable "policy_permissions" {
-    description = "The permissions of the default policy of the role created"
-    type = map
+  description = "The permissions of the default policy of the role created"
+  type        = map(any)
 }
 
 variable "assume_role_type" {
-    description = "Policy allowing a service to assume a role"
-    type = string
-    default = "Service"
+  description = "Policy allowing a service to assume a role"
+  type        = string
+  default     = "Service"
 }
 
 variable "assume_role_identifiers" {
-    description = "Policy allowing a service to assume a role"
-    type = list
-    default = ["ec2.amazonaws.com", "lambda.amazonaws.com"]
+  description = "Policy allowing a service to assume a role"
+  type        = list(any)
+  default     = ["ec2.amazonaws.com", "lambda.amazonaws.com"]
 }
